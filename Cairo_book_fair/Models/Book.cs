eedf1e82787a729129d1b39f699cdca2bb191c08
reefaey <abdallah.hussien101@gmail.com>
2024-06-05 02:32:24 +0300

    Add some models

---
+++ Cairo_book_fair/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cairo_book_fair.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+        [ForeignKey("Hall")]
+        public int HallNumber { get; set; }
+        public Hall Hall { get; set; }
+        [ForeignKey("Stage")]
+        public string StageName { get; set; }
+        public Stage Stage { get; set; }
+        public string ImageUrl { get; set; }
+        public string PublishingYear { get; set; }
+        public int PagesNumber { get; set; }
+        public List<string> Category { get; set; }
+        public string SoundBook { get; set; }
+    }
+}
---
+++ Cairo_book_fair/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Cairo_book_fair.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BooksNumber {  get; set; }
+        public List<Book>? Books { get; set;}
+    }
+}
