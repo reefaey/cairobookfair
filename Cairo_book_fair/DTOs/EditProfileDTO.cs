6d3c4cff702246dedc2990cf4e9a31021eabb595
reefaey <abdallah.hussien101@gmail.com>
2024-06-20 10:28:18 +0300

    Finish Account

---
+++ Cairo_book_fair/DTOs/EditProfileDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Cairo_book_fair.DTOs
+{
+    public class EditProfileDTO
+    {
+        public string Name { get; set; }
+        public string Bio { get; set; }
+
+        //Birthday
+        public string? ProfileImage { get; set; }
+        public string Location { get; set; }
+    }
+}
