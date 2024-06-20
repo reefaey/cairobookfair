82a4fd28c04642a61b3ae7542998700e304adab1
esraamohammed44 <esraamohammed1112000@gmail.com>
2024-06-20 23:04:19 +0200

    Add Ticket

---
+++ Cairo_book_fair/Controllers/TicketController.cs
@@ -0,0 +1,21 @@
+﻿using Cairo_book_fair.DTOs;
+using Cairo_book_fair.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Cairo_book_fair.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private readonly ITicketService _ticketService;
+
+        [HttpGet]
+        public ActionResult<IEnumerable<TicketDTO>> GetTickets()
+        {
+            List<TicketDTO> ticketDTOs = _ticketService.GetAllDTO();
+            return Ok(ticketDTOs);
+        }
+    }
+}
