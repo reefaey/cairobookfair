690a76b213fb3ae9190a2424e5d01503774997bd
esraamohammed44 <esraamohammed1112000@gmail.com>
2024-06-21 17:05:25 +0200

    Edit Class Hall & Publisher

--- Cairo_book_fair/Models/Publisher.cs
+++ Cairo_book_fair/Models/Publisher.cs
@@ -12,7 +12,7 @@ namespace Cairo_book_fair.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int NumberOfBooks { get; set; }
-
+        public string? ImageUrl { get; set; }
         [ForeignKey("Block")]
         public int BlockId { get; set; }
         public Block Block { get; set; }
