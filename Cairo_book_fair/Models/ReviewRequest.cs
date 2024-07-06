a54ed093b9c98a8305dcc4574d2b4e96d2163653
reefaey <abdallah.hussien101@gmail.com>
2024-07-06 13:10:28 +0300

    Enum and request review

---
+++ Cairo_book_fair/Models/ReviewRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cairo_book_fair.Models
+{
+    public class ReviewRequest
+    {
+        public int Id { get; set; }
+
+        public 
+
+        [ForeignKey("Review")]
+        public int ReviewId { get; set; }
+        public Review Review { get; set; }
+
+        [ForeignKey("User")]
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+}
