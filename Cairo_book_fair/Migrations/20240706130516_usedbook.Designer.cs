cf55ce92f1bac9004ef287d1e5ed745b8b4de48d
reefaey <abdallah.hussien101@gmail.com>
2024-07-06 16:09:46 +0300

    Edit conection string

--- Cairo_book_fair/Migrations/20240706130516_usedbook.Designer.cs
+++ Cairo_book_fair/Migrations/20240706130516_usedbook.Designer.cs
@@ -4,6 +4,7 @@ using Cairo_book_fair.DBContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cairo_book_fair.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20240706130516_usedbook")]
+    partial class usedbook
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
